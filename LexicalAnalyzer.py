d766ce5f94fb32af0b2a370b71091091f0e9a6e6
Bregoli <viniciusbregoli@hotmail.com>
2024-04-08 16:51:48 -0300

    Add memory and result tracking to LexicalAnalyzer class

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -6,6 +6,8 @@ class LexicalAnalyzer:
     def __init__(self, filepath):  # Constructor
         self.filepath = filepath
         self.lines = self.analyzeFile()
+        self.MEM = 0
+        self.RES = []
         self.trees = list(self.buildTree())
 
     def classifyToken(self, token):  # Classify the tokens
@@ -31,9 +33,45 @@ class LexicalAnalyzer:
             file.close()
         return lines
 
+    def solveLine(self, line):  # Solve the line
+        tokens = self.analyze(line)
+        stack = []
+        i = 0
+        for token, type in tokens:
+            if type == "NUMBER":
+                stack.append(float(token))
+            elif type == "MEMORY":
+                try:
+                    nextToken, nextType = tokens[i + 1]
+                    if nextType == "OPERATOR" or nextType == "NUMBER":
+                        stack.append(self.MEM)
+                except IndexError:
+                    self.MEM = stack.pop()
+                    return 0
+            elif type == "RES":
+                left = stack.pop()
+                stack.append(self.RES[-int(left)])
+            elif type == "OPERATOR":
+                right = stack.pop()
+                left = stack.pop()
+                if token == "+":
+                    stack.append(left + right)
+                elif token == "-":
+                    stack.append(left - right)
+                elif token == "*":
+                    stack.append(left * right)
+                elif token == "/":
+                    stack.append(left / right)
+                elif token == "%":
+                    stack.append(left % right)
+                elif token == "^":
+                    stack.append(left**right)
+            i += 1
+        return stack.pop()
+
     def buildTree(self):
-        MEM = 0
         for line in self.lines:
+            self.RES.append(self.solveLine(line))
             tokens = self.analyze(line)
             stack = []
             i = 0
@@ -49,17 +87,23 @@ class LexicalAnalyzer:
                         try:
                             right = stack.pop()
                             left = stack.pop()
-                            stack.append(TreeNode(token, type="MEMORY", children=[left, right]))
+                            stack.append(
+                                TreeNode(token, type="MEMORY", children=[left, right])
+                            )
                         except IndexError:
-                            MEM = right
-                            stack.append(TreeNode(token, type="MEMORY", children=[right]))
+                            stack.append(
+                                TreeNode(token, type="MEMORY", children=[right])
+                            )
+                elif type == "RES":
+                    left = stack.pop()
+                    stack.append(TreeNode(f"{left} {token}", type="RES"))
                 elif type == "OPERATOR":
                     right = stack.pop()
                     left = stack.pop()
                     node = TreeNode(token, type="OPERATOR", children=[left, right])
                     stack.append(node)
                 i += 1
-            yield stack.pop()  # Retorna a raiz da AST para a linha atual
+            yield stack.pop()
 
 
 lex = LexicalAnalyzer("./misc/input.txt")
@@ -68,3 +112,4 @@ for index, ast in enumerate(lex.trees):
     ast_graph.render(
         filename=f"{index + 1}_ast", directory="./tree_imgs", format="png", cleanup=True
     )
+print(lex.RES)
